9a63f8adc9b62d3ed3594c221441f89d81d2bb2f
richardtam.ny <richardtam.ny@gmail.com>
2018-11-27 23:39:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ function removeLastKitten() {
 }
 
 function removeFirstKitten() {
-  removeFirstKitten = kittens.slice(1, kittens.length -1)
+  removeFirstKitten = kittens.slice(1, kittens.length -2)
   return removeFirstKitten
 }
